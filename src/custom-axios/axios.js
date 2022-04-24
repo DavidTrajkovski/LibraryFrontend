2df651b15ebfb5eea0eb4d5b121279d7ef709a5d
DavidTrajkovski <davidtrajkovski11@gmail.com>
2022-04-24 22:44:47 +0200

    commit4

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://emt-david-l2-library-backend.herokuapp.com/api',
+    baseURL: 'https://emt-lab2-191027-backend.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
